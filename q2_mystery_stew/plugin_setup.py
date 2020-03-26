87083e11ad063be9f0ec6a3440d075f185909532
Anthony <oddant1@hotmail.com>
2020-03-26 11:40:48 -0700

    SQUASH: Begin generating usage examples

--- q2_mystery_stew/plugin_setup.py
+++ q2_mystery_stew/plugin_setup.py
@@ -9,7 +9,8 @@ from qiime2.plugin import Plugin
 from itertools import product
 from collections import namedtuple
 
-from qiime2.plugin import Int, Range
+from qiime2.plugin import Int, Range, UsageAction, UsageInputs, \
+                          UsageOutputNames
 
 import q2_mystery_stew
 from q2_mystery_stew.template import rewrite_function_signature, \
@@ -30,11 +31,31 @@ plugin = Plugin(
 )
 
 Sig = namedtuple('Sig', ['num_inputs', 'num_outputs', 'signature'])
-Param = namedtuple('Param', ['name', 'type', 'domain'])
+Param = namedtuple('Param', ['base_name', 'type', 'domain'])
 
 function_signatures = (function_template_1output,
                        function_template_2output,
                        function_template_3output)
+
+
+class UsageInstantiator:
+    def __init__(self, inputs_params, outputs, name):
+        self.inputs_params = {}
+        for param in inputs_params:
+            self.inputs_params.update(param)
+
+        self.outputs = {}
+        for output in outputs:
+            self.outputs.update({output[0]: str(output[1])})
+
+        self.name = name
+
+    def __call__(self, use):
+        use.action(
+            UsageAction(plugin_id='mystery_stew', action_id=self.name),
+            UsageInputs(**self.inputs_params),
+            UsageOutputNames(**self.outputs),
+        )
 
 
 def generate_signatures(args):
@@ -47,9 +68,9 @@ def generate_signatures(args):
 int_args = {
     'single_int': Param('single_int', Int, (-4, 0, 4)),
     'int_range_one_arg': Param('int_range_one_arg',
-                               Int % Range(5), (-4, 0, 4)),
+                               Int % Range(5), (-5, 1, 5)),
     'int_range_two_args': Param('int_range_two_args',
-                                Int % Range(-5, 5), (-4, 0, 4))
+                                Int % Range(-5, 5), (-6, 2, 6))
 }
 
 num_functions = 0
@@ -58,27 +79,46 @@ signatures = generate_signatures(int_args)
 for signature in signatures:
     for params in product(int_args.values(),
                           repeat=signature.num_inputs):
+        action_name = f'func_{num_functions}'
+
         param_dict = {}
         for i, param in enumerate(params):
-            param_dict.update({param.name + f'__{i}': param.type})
+            param_dict.update({param.base_name + f'_{i}': param})
+
+        # This dictionary allows for easy registration of Qiime parameters
+        param_name_to_type_dict = \
+            {name: value.type for name, value in param_dict.items()}
 
         rewrite_function_signature(signature.signature,
                                    {},
-                                   param_dict,
+                                   param_name_to_type_dict,
                                    signature.num_outputs,
-                                   f'func_{num_functions}')
+                                   action_name)
 
-        output = []
+        outputs = []
         for i in range(signature.num_outputs):
-            output.append((str(i), EchoOutput))
+            outputs.append((f'outputs_{i}', EchoOutput))
+
+        valid_param_values = [[] for param in range(len(param_dict))]
+        for index, name in enumerate(param_dict):
+            for valid_param_value in param_dict[name].domain:
+                valid_param_values[index].append({name: valid_param_value})
+
+        usage_examples = {}
+        for example_number, valid_params in \
+                enumerate(product(*valid_param_values)):
+            example_name = action_name + f'_{example_number}'
+            usage_examples[example_name] = \
+                UsageInstantiator(valid_params, outputs, action_name)
 
         plugin.methods.register_function(
             function=signature.signature,
             inputs={},
-            parameters=param_dict,
-            outputs=output,
-            name=f'func_{num_functions}',
-            description=''
+            parameters=param_name_to_type_dict,
+            outputs=outputs,
+            name=action_name,
+            description='',
+            examples=usage_examples
         )
         num_functions += 1
 
--- q2_mystery_stew/test/test_templates.py
+++ q2_mystery_stew/test/test_templates.py
@@ -6,50 +6,16 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 import unittest
-from itertools import product
-from collections import namedtuple
 
-from qiime2.plugin import Int, Range
+from qiime2.sdk import usage
 
 from q2_mystery_stew.plugin_setup import plugin
-from q2_mystery_stew.templatable_echo_fmt import EchoOutputFmt
-
-Param = namedtuple('Param', ['name', 'type', 'domain'])
 
 
 class TestTemplates(unittest.TestCase):
     def test_int_cases(self):
-        int_args = {
-            'single_int': Param('single_int', Int, (-4, 0, 4)),
-            'int_range_one_arg': Param('int_range_one_arg',
-                                       Int % Range(5), (-4, 0, 4)),
-            'int_range_two_args': Param('int_range_two_args',
-                                        Int % Range(-5, 5), (-4, 0, 4))
-        }
+        use = usage.DiagnosticUsage()
 
-        for method in plugin.methods.values():
-            signature = method.signature
-            args = []
-
-            for param in signature.parameters:
-                param_type = param.split('__')[0]
-                args.append(int_args[param_type].domain)
-
-            for arg_list in product(*args):
-                output = method(*arg_list)
-
-                with output[0].view(EchoOutputFmt).open() as fh:
-                    lines = ''.join(line for line in fh)
-
-                    for arg, param_name in zip(arg_list, signature.parameters):
-                        self.assertIn(f'{param_name}: {arg}', lines)
-
-                if len(output) > 1:
-                    with output[1].view(EchoOutputFmt).open() as fh:
-                        lines = ''.join(line for line in fh)
-                        self.assertIn('second', lines)
-
-                if len(output) > 2:
-                    with output[2].view(EchoOutputFmt).open() as fh:
-                        lines = ''.join(line for line in fh)
-                        self.assertIn('third', lines)
+        for action_ in plugin.actions.values():
+            for example in action_.examples.values():
+                example(use)
