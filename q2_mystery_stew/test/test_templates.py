f772006f4d400186d47bcad44869cd66ce8fc777
Anthony Simard <als872@nau.edu>
2020-10-27 15:22:51 -0700

    Make tests runable

--- q2_mystery_stew/test/test_templates.py
+++ q2_mystery_stew/test/test_templates.py
@@ -7,9 +7,11 @@
 # ----------------------------------------------------------------------------
 from qiime2.plugin.testing import TestPluginBase
 
+from q2_mystery_stew.plugin_setup import create_plugin
 
 class TestTemplates(TestPluginBase):
     package = 'q2_mystery_stew.test'
+    plugin = create_plugin()
 
     def test_examples(self):
         self.execute_examples()
