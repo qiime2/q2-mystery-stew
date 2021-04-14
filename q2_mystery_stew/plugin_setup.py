11a67976402491fd7368e776d6413ea1f1ac06c2
Evan Bolyen <ebolyen@gmail.com>
2021-04-13 17:09:21 -0700

    TST: add more artifacts (#22)

--- q2_mystery_stew/plugin_setup.py
+++ q2_mystery_stew/plugin_setup.py
@@ -205,21 +205,38 @@ class UsageInstantiator:
             )
 
 
-def single_int1():
+def single_int1_1():
     return qiime2.Artifact.import_data('SingleInt1', 42)
 
 
-def single_int2():
+def single_int1_2():
+    return qiime2.Artifact.import_data('SingleInt1', 43)
+
+
+def single_int1_3():
+    return qiime2.Artifact.import_data('SingleInt1', 44)
+
+
+def single_int2_1():
     return qiime2.Artifact.import_data('SingleInt2', 2019)
+
+
+def single_int2_2():
+    return qiime2.Artifact.import_data('SingleInt2', 2020)
+
+
+def single_int2_3():
+    return qiime2.Artifact.import_data('SingleInt2', 2021)
 
 
 def artifact_params():
     yield ParamTemplate('simple_type1', SingleInt1, SingleIntFormat,
-                        (single_int1,))
+                        (single_int1_1, single_int1_2, single_int1_3))
     yield ParamTemplate('simple_type2', SingleInt2, SingleIntFormat,
-                        (single_int2,))
+                        (single_int2_1, single_int2_2, single_int2_3))
     yield ParamTemplate('union_type', SingleInt1 | SingleInt2, SingleIntFormat,
-                        (single_int1, single_int2))
+                        (single_int1_1, single_int2_1, single_int2_2,
+                         single_int1_3))
 
 
 def metadata1():
