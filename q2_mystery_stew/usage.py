67b8ec0f288bd333f845e7847a09fbf14cacd7cd
Matthew Dillon <matthewrdillon@gmail.com>
2021-10-12 13:03:07 -0700

    maint: usage v2

--- q2_mystery_stew/usage.py
+++ q2_mystery_stew/usage.py
@@ -38,24 +38,25 @@ class UsageInstantiator:
             elif is_semantic_type(template.qiime_type):
                 if type(argument) == list or type(argument) == set:
                     collection_type = type(argument)
-                    input_temp = []
                     transformed_inputs[name] = collection_type()
+                    inputs[name] = collection_type()
 
                     for arg in argument:
-                        input_temp.append(use.init_data(arg.__name__, arg))
                         artifact = arg()
                         view = artifact.view(template.view_type)
                         view.__hide_from_garbage_collector = artifact
 
                         if collection_type == list:
                             transformed_inputs[name].append(view)
+                            inputs[name].append(
+                                use.init_artifact(arg.__name__, arg))
                         elif collection_type == set:
                             transformed_inputs[name].add(view)
-                        inputs[name] = \
-                            use.init_data_collection(name, collection_type,
-                                                     *input_temp)
+                            inputs[name].add(
+                                use.init_artifact(arg.__name__, arg))
                 else:
-                    inputs[name] = use.init_data(argument.__name__, argument)
+                    inputs[name] = use.init_artifact(argument.__name__,
+                                                     argument)
                     artifact = argument()
                     view = artifact.view(template.view_type)
                     view.__hide_from_garbage_collector = artifact
@@ -73,41 +74,44 @@ class UsageInstantiator:
                     inputs[name] = md_rec
                     transformed_inputs[name] = md
                 else:
-                    inputs[name] = use.get_metadata_column(column_name, md_rec)
+                    inputs[name] = use.get_metadata_column(
+                        '%s_%s' % (factory.__name__, column_name),
+                        column_name,
+                        md_rec,
+                    )
                     transformed_inputs[name] = md.get_column(column_name)
 
             else:
                 inputs[name] = transformed_inputs[name] = argument
 
-        use.action(
+        computed_results = use.action(
             use.UsageAction(plugin_id='mystery_stew', action_id=self.name),
             use.UsageInputs(**inputs),
             use.UsageOutputNames(**self.output_names),
         )
 
         for idx, (output_name, expected_type) in enumerate(self.outputs):
-            self._assert_output(use, transformed_inputs, self.defaults,
-                                output_name, expected_type, idx)
+            self._assert_output(computed_results, transformed_inputs,
+                                self.defaults, output_name, expected_type, idx)
 
     @staticmethod
     def _fmt_regex(name, arg):
         line = argument_to_line(name, arg).strip()
         return re.escape(line)
 
-    def _assert_output(self, use, inputs, defaults, output_name, expected_type,
-                       idx):
+    def _assert_output(self, computed_results, inputs, defaults, output_name,
+                       expected_type, idx):
+        output = computed_results[idx]
         if idx == 0:
             for input_name, input_arg in chain(inputs.items(),
                                                defaults.items()):
                 regex = self._fmt_regex(input_name, input_arg)
-                output = use.get_result(output_name)
                 output.assert_has_line_matching(
                     label='<generated>',
                     path='echo.txt',
                     expression=regex
                 )
         else:
-            output = use.get_result(output_name)
             output.assert_has_line_matching(
                 label='<generated>',
                 path='echo.txt',
