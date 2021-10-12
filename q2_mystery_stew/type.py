7e68c363153863321e979f991b4a05dcf1a32696
Matthew Dillon <matthewrdillon@gmail.com>
2021-10-12 13:03:07 -0700

    maint: usage api v2
    Co-authored-by: Evan Bolyen <ebolyen@gmail.com>
    Co-authored-by: Keegan Evans <45274931+Keegan-Evans@users.noreply.github.com>

--- q2_mystery_stew/type.py
+++ q2_mystery_stew/type.py
@@ -8,12 +8,18 @@
 
 from qiime2.plugin import SemanticType
 
+
 EchoOutput = SemanticType('EchoOutput')
+EchoOutputBranch1 = SemanticType('EchoOutputBranch1')
+EchoOutputBranch2 = SemanticType('EchoOutputBranch2')
+EchoOutputBranch3 = SemanticType('EchoOutputBranch3')
+
+
 SingleInt1 = SemanticType('SingleInt1')
 SingleInt2 = SemanticType('SingleInt2')
 
+
 IntWrapper = SemanticType('IntWrapper', field_names='first')
 _variant = IntWrapper.field['first']
-
 WrappedInt1 = SemanticType('WrappedInt1', variant_of=_variant)
 WrappedInt2 = SemanticType('WrappedInt2', variant_of=_variant)
