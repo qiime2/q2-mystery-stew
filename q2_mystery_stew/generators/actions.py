e13f715e1ab5d42cf34e4eadc31290068a299f17
Evan Bolyen <ebolyen@gmail.com>
2021-10-12 17:41:06 -0700

    REF: factor action-gen out from param-gen

---
+++ q2_mystery_stew/generators/actions.py
@@ -0,0 +1,51 @@
+from collections import deque
+
+from qiime2.sdk.util import is_metadata_type, is_semantic_type
+
+from q2_mystery_stew.type import EchoOutput
+from q2_mystery_stew.generators.base import ActionTemplate, Invocation
+
+
+def generate_single_type_methods(generator):
+    for idx, param in enumerate(generator, 1):
+        action_id = f'test_{generator.__name__}_{idx}'
+        qiime_outputs = [('only_output', EchoOutput)]
+        specs = {}
+        defaults = {}
+
+        primary = param.mint_spec()
+        specs[primary.name] = primary
+
+        optional = param.mint_spec(prefix='optional_', default=None)
+        specs[optional.name] = optional
+        defaults[optional.name] = optional.default
+
+        if (not is_metadata_type(param.qiime_type)
+                and not is_semantic_type(param.qiime_type)):
+            for domain_idx, arg in enumerate(param.domain):
+                default = param.mint_spec(f'default{domain_idx}_', default=arg)
+                specs[default.name] = default
+                defaults[default.name] = default.default
+
+        # USAGE: pass each value in the domain for required args
+        domain = [Invocation({primary.name: arg}, qiime_outputs)
+                  for arg in param.domain]
+
+        # USAGE: pass all defaults manually
+        arg = param.domain[-1]
+        domain.append(Invocation({primary.name: arg, **defaults},
+                                 qiime_outputs))
+
+        # USAGE: pass a different value to all defaults
+        if len(defaults) > 1:
+            # if len(domain) is 1, then there would be nothing to rotate and
+            # the arguments would be the same as the default
+            shifted = deque([arg, arg, *param.domain])
+            shifted.rotate(1)
+            domain.append(Invocation({k: v for k, v in zip(specs, shifted)},
+                                     qiime_outputs))
+
+        yield ActionTemplate(action_id=action_id,
+                             parameter_specs=specs,
+                             registered_outputs=qiime_outputs,
+                             invocation_domain=domain)
--- q2_mystery_stew/generators/base.py
+++ q2_mystery_stew/generators/base.py
@@ -5,8 +5,36 @@
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
-
+from inspect import Parameter
 from collections import namedtuple
 
-ParamTemplate = namedtuple('ParamTemplate', ['base_name', 'qiime_type',
-                                             'view_type', 'domain'])
+
+class ParamSpec:
+    NoDefault = Parameter.empty
+
+    def __init__(self, name, qiime_type, view_type, default):
+        self.name = name
+        self.qiime_type = qiime_type
+        self.view_type = view_type
+        self.default = default
+
+
+class ParamTemplate:
+    def __init__(self, base_name, qiime_type, view_type, domain):
+        self.base_name = base_name
+        self.qiime_type = qiime_type
+        self.view_type = view_type
+        self.domain = domain
+
+    def mint_spec(self, prefix='', default=ParamSpec.NoDefault):
+        return ParamSpec(prefix+self.base_name,
+                         self.qiime_type,
+                         self.view_type,
+                         default)
+
+
+Invocation = namedtuple('Invocation', ['kwargs', 'expected_output_types'])
+ActionTemplate = namedtuple('ActionTemplate', ['action_id',
+                                               'parameter_specs',
+                                               'registered_outputs',
+                                               'invocation_domain'])
