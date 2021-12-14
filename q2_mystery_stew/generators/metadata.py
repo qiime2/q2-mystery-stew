b6cd9ea8ce9bbe172c4de28402f6d45359f21972
Evan Bolyen <ebolyen@gmail.com>
2021-12-14 13:43:18 -0700

    NEW: adds metadata view and metadata merge

--- q2_mystery_stew/generators/metadata.py
+++ q2_mystery_stew/generators/metadata.py
@@ -9,7 +9,7 @@
 import pandas as pd
 
 import qiime2
-from qiime2.plugin import Metadata, MetadataColumn, Categorical
+from qiime2.plugin import Metadata, MetadataColumn, Categorical, Numeric
 
 from q2_mystery_stew.generators.base import ParamTemplate
 
@@ -21,9 +21,37 @@ def metadata1():
     return qiime2.Metadata(df)
 
 
+def metadata2():
+    df = pd.DataFrame({'col3': [1, 2, 3], 'col4': [0.1, 0.01, 0.001]},
+                      index=['id1', 'id2', 'id3'])
+    df.index.name = 'id'
+    return qiime2.Metadata(df)
+
+
+def artifact_cat():
+    return qiime2.Artifact.import_data('BasicallyMetadata', metadata1())
+
+
+def artifact_num():
+    return qiime2.Artifact.import_data('BasicallyMetadata', metadata2())
+
+
 def metadata_params():
-    yield ParamTemplate('metadata_cat', Metadata, qiime2.Metadata,
-                        (metadata1,))
+    yield ParamTemplate('metadata', Metadata, qiime2.Metadata,
+                        (metadata1, metadata2, artifact_cat, artifact_num,
+                         [metadata1, metadata2],
+                         [metadata1, artifact_num],
+                         [artifact_cat, metadata2],
+                         [artifact_cat, artifact_num]))
     yield ParamTemplate('column_cat', MetadataColumn[Categorical],
                         qiime2.CategoricalMetadataColumn,
-                        ([metadata1, 'col1'], [metadata1, 'col2']))
+                        ([metadata1, 'col1'], [metadata1, 'col2'],
+                         [artifact_cat, 'col1'], [artifact_cat, 'col2']))
+    yield ParamTemplate('column_num', MetadataColumn[Numeric],
+                        qiime2.NumericMetadataColumn,
+                        ([metadata2, 'col3'], [metadata2, 'col4'],
+                         [artifact_num, 'col3'], [artifact_num, 'col4']))
+    yield ParamTemplate('column_any', MetadataColumn[Categorical | Numeric],
+                        qiime2.MetadataColumn,
+                        ([metadata1, 'col1'], [metadata2, 'col4'],
+                         [artifact_cat, 'col2'], [artifact_num, 'col3']))
