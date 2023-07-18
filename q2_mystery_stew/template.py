3bfa424353f0c85d52c36e053a9aef8e85d6ce38
Anthony <als872@nau.edu>
2023-07-18 16:02:43 -0700

    Start templating output collcetions

--- q2_mystery_stew/template.py
+++ q2_mystery_stew/template.py
@@ -14,7 +14,7 @@ import qiime2
 from q2_mystery_stew.format import SingleIntFormat, EchoOutputFmt
 
 
-def get_disguised_echo_function(id, python_parameters, num_outputs):
+def get_disguised_echo_function(id, python_parameters, qiime_outputs):
     TEMPLATES = [
         _function_template_1output,
         _function_template_2output,
@@ -23,8 +23,32 @@ def get_disguised_echo_function(id, python_parameters, num_outputs):
         _function_template_5output,
     ]
 
-    function = TEMPLATES[num_outputs - 1]
-    disguise_echo_function(function, id, python_parameters, num_outputs)
+    # raise ValueError(qiime_outputs)
+    # raise ValueError(str(qiime_outputs[0][1]))
+    # qiime_outputs is a list of (name, type) tuples. If all output types are
+    # EchoFormat, get template based on number of outputs
+    types = []
+    for output in qiime_outputs:
+        _, _type = output
+        types.append(str(_type))
+
+    if all(_type == 'EchoOutput' for _type in types):
+        function = TEMPLATES[len(qiime_outputs) - 1]
+    # Otherwise if we only have have one output it must be a
+    # Collection[EchoOutput]
+    elif len(qiime_outputs) == 1:
+        function = _function_template_collection_only
+    # Otherwise, we need to figure out if the Collection is first or second in
+    # the outputs
+    else:
+        # Check if type of first output is collection
+        if str(qiime_outputs[0][1]) == 'Collection[EchoOutput]':
+            function = _function_template_collection_first
+        # Otherwise, it must be the second
+        else:
+            function = _function_template_collection_second
+
+    disguise_echo_function(function, id, python_parameters, len(qiime_outputs))
 
     return function
 
@@ -88,18 +112,53 @@ def argument_to_line(name, arg):
     return json.dumps([name, value, expected_type]) + '\n'
 
 
-def _echo_outputs(kwargs, num_outputs):
-    output = EchoOutputFmt()
-    with output.open() as fh:
-        for name, arg in kwargs.items():
-            fh.write(argument_to_line(name, arg))
+def _echo_outputs(kwargs, num_outputs, collection_idx=None):
+    """ NOTE: We used 1 based indexing here for reasons I do not remember, so
+        collection_idx uses 1 based indexing.
+    """
+    outputs = []
 
-    if num_outputs > 1:
-        output = (output, *map(lambda x: EchoOutputFmt(),
-                               range(1, num_outputs)))
-        for idx, fmt in enumerate(output[1:], 2):
-            with fmt.open() as fh:
-                fh.write(str(idx))
+    if collection_idx == 1:
+        output = _echo_collection(kwargs=kwargs)
+    else:
+        output = _echo_single(kwargs=kwargs)
+
+    outputs.append(output)
+
+    # We already handled the 1st output above, and we do 1 based indexing
+    for idx in range(2, num_outputs + 1):
+        if idx == collection_idx:
+            output = _echo_collection(idx=idx)
+        else:
+            output = _echo_single(idx=idx)
+
+        outputs.append(output)
+
+    return tuple(outputs)
+
+
+def _echo_collection(kwargs=None, idx=None):
+    outputs = {}
+
+    if kwargs:
+        for name, arg in kwargs.items():
+            outputs[name] = _echo_single(kwargs={name: arg})
+    else:
+        for i in range(2):
+            outputs[i] = _echo_single(kwargs={idx: i})
+
+    return outputs
+
+
+def _echo_single(kwargs=None, idx=None):
+    output = EchoOutputFmt()
+
+    with output.open() as fh:
+        if kwargs:
+            for name, arg in kwargs.items():
+                fh.write(argument_to_line(name, arg))
+        else:
+            fh.write(str(idx))
 
     return output
 
@@ -122,3 +181,15 @@ def _function_template_4output(**kwargs):
 
 def _function_template_5output(**kwargs):
     return _echo_outputs(kwargs, 5)
+
+
+def _function_template_collection_only(**kwargs):
+    return _echo_outputs(kwargs, 1, 1)
+
+
+def _function_template_collection_first(**kwargs):
+    return _echo_outputs(kwargs, 2, 2)
+
+
+def _function_template_collection_second(**kwargs):
+    return _echo_outputs(kwargs, 2, 2)
